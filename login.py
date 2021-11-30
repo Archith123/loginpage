426b8fb721ac8ed176e4783975d97a76c29213da
Archith123 <1ds19is021@dsce.edu.in>
2021-11-30 18:44:19 +0530

    initial commit

---
+++ login.py
@@ -0,0 +1,11 @@
+print('create new account')
+username=input('enter the username: ')
+password=input('enter the password: ')
+print('your account created succesfully')
+print('login now!')
+username2=input('enter username: ')
+password2=input('enter password: ')
+if username==username2 and password==password2 :
+    print('logged in succesfully!!!')
+else:
+    print('invalid credentials')
